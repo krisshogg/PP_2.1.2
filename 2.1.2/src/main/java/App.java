9acb07c97c131e0c770cfe2096d567fe64d4eeea
DESKTOP-L7BTGRO\missp <helooooooo@yamdex.ru>
2023-01-07 16:14:43 +0700

    init commit

---
+++ 2.1.2/src/main/java/App.java
@@ -0,0 +1,25 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println("helloworld: " + (bean1 == bean2));
+        System.out.println("cat: " + (bean3 == bean4));
+
+
+    }
+}
